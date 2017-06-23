67986c9933af5e60b13e7d0d037cb7bb2f7342a9
Gabriel Nunez <gabriel.nzn@gmail.com>
2017-06-23 09:23:18 -0400

    Desarrollo de la pagina Error 404

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Ticketing' });
+  res.render('index',{ title: 'Ticketing', bodyBackground: '' });
 });
 
 module.exports = router;
