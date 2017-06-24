d5c66f3f9b8a4c442c1bcf97cba5fa4b648edf0a
Gabriel Nunez <gabriel.nzn@gmail.com>
2017-06-23 20:52:55 -0400

    Cambiando el tipo de elemento de las opciones

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,23 @@
 $(document).foundation()
+
+function openOption(ev, optionName) {
+  console.log(optionName)
+
+  // Esconde todos los elementos con la clase tabcontent
+  const tabcontent = document.querySelectorAll('.tabcontent')
+  
+  tabcontent.forEach((content) => {
+    content.style.display = 'none'
+  })
+
+  // Quita la clase activa del tab que la tenga
+  const tablinks = document.querySelectorAll('.tablinks')
+  
+  tablinks.forEach((tab) => {
+    tab.className = tab.className.replace(' active', '')
+  })
+
+  // Muestra el contenido del tab seleccionado, y agrega la clase activa al tab que corresponde
+  // document.getElementsByClassName(optionName).style.display = 'block'
+  ev.currentTarget.className += ' active'
+}
