a4131111f34d3e95cc6000a4fa71d21dc95a3cd6
Gabriel Nunez <gabriel.nzn@gmail.com>
2017-06-25 14:36:27 -0400

    Maquetando el formulario de datos obligatorios, de la pagina Agregar evento

--- public/js/app.js
+++ public/js/app.js
@@ -1,14 +1,21 @@
 $(document).foundation()
 
-function openOption(ev, optionName) {
-  console.log(optionName)
-
-  // Esconde todos los elementos con la clase tabcontent
-  const tabcontent = document.querySelectorAll('.tabcontent')
+function toggleContent(contentId) {
+  // Esconde todos los elementos con la clase content
+  const content = document.querySelectorAll('.content')
   
-  tabcontent.forEach((content) => {
+  content.forEach((content) => {
     content.style.display = 'none'
   })
+
+  // Muestra el contenido deseado
+  document.getElementById(contentId).style.display = 'block'
+}
+
+// Funcion que es invocada al seleccionar una opcion de la pagina principal
+function openOption(ev, contentId) {
+
+  toggleContent(contentId)
 
   // Quita la clase activa del tab que la tenga
   const tablinks = document.querySelectorAll('.tablinks')
@@ -17,7 +24,11 @@ function openOption(ev, optionName) {
     tab.className = tab.className.replace(' active', '')
   })
 
-  // Muestra el contenido del tab seleccionado, y agrega la clase activa al tab que corresponde
-  document.getElementById(optionName).style.display = 'block'
+  // Agrega la clase activa al tab que corresponde
   ev.currentTarget.className += ' active'
+}
+
+// Funcion que es invocada al seleccionar la opcion de agregar evento
+function addEvent(ev, contentId) {
+  toggleContent(contentId)
 }
