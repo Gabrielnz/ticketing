7e02df5d56143cc4d0ccee84bbfbb12e4a0b2223
Gabriel Nunez <gabriel.nzn@gmail.com>
2017-06-28 05:10:16 -0400

    Utilizar datos estaticos

--- routes/index.js
+++ routes/index.js
@@ -11,7 +11,46 @@ router.get('/', function(req, res, next) {
   	{
   		title: 'Recaudia - Dashboard',
   		bodyBackground: '',
-  		activeTab: 'dashboard'
+  		activeTab: 'dashboard',
+      cards: {
+        sold_tickets: 300,
+        raised_money: "600.000",
+        events_held: 5,
+        sold_tickets_percentage: 75
+      },
+      active_events:
+      [
+        {
+          name: '438204823904890238903890284908402398490',
+          date: '27/06/17',
+          total: '100/500',
+          amount: '150.000'
+        },
+        {
+          name: '438204823904890238903890284908402398490',
+          date: '27/06/17',
+          total: '100/500',
+          amount: '150.000'
+        },
+        {
+          name: '438204823904890238903890284908402398490',
+          date: '27/06/17',
+          total: '100/500',
+          amount: '150.000'
+        },
+        {
+          name: '438204823904890238903890284908402398490',
+          date: '27/06/17',
+          total: '100/500',
+          amount: '150.000'
+        },
+        {
+          name: '438204823904890238903890284908402398490',
+          date: '27/06/17',
+          total: '100/500',
+          amount: '150.000'
+        }
+      ]
   	});
   next();
 });
@@ -21,7 +60,42 @@ router.get('/events', function(req, res, next) {
   	{
   		title: 'Recaudia - Eventos',
   		bodyBackground: '',
-  		activeTab: 'events'
+  		activeTab: 'events',
+      events:
+      [
+        {
+          show: true,
+          name: '43820482390489023389 0284908 402398490',
+          date: '27/06/17',
+          seats: '100/500',
+          amount: '150.000',
+          status: 'in-progress'
+        },
+        {
+          show: false,
+          name: '43820482390489023389 0284908 402398490',
+          date: '27/06/17',
+          seats: '100/500',
+          amount: '150.000',
+          status: 'in-progress'
+        },
+        {
+          show: true,
+          name: '43820482390489023389 0284908 402398490',
+          date: '27/06/17',
+          seats: '100/500',
+          amount: '150.000',
+          status: 'done'
+        },
+        {
+          show: false,
+          name: '43820482390489023389 0284908 402398490',
+          date: '27/06/17',
+          seats: '100/500',
+          amount: '150.000',
+          status: 'done'
+        }
+      ]
   	});
   next();
 });
